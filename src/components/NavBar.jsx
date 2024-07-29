4cb2e621672b2b79504bc5525896acaad15c634f
Lucas Camisassa <l.e.camisassa@gmail.com>
2024-07-28 21:37:34 -0300

    PreEntrega2+Camisassa(react)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import imagenCarrito from "../img/imagenCarrito.png";
 import CartWidget from "../components/CartWidget"
+import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function NavBar() {
     return (
         <>
         <div className="d-flex container navbar-styles">
-            <img className="img-fluid img-logo-style" src={imagenCarrito} alt="logo-tienda" />
+            <NavLink className="d-flex" to="/"><img className="img-logo-style" src={imagenCarrito} alt="logo-tienda" /></NavLink>
             <ul className="d-flex container justify-content-around align-items-center list-unstyled m-1">
-                <li><a className="text-decoration-none li-navbar-style" href="http://" target="_blank" rel="noopener noreferrer">HOME</a></li>
-                <li><a className="text-decoration-none li-navbar-style" href="http://" target="_blank" rel="noopener noreferrer">PRODUCTOS</a></li>
-                <li><a className="text-decoration-none li-navbar-style" href="http://" target="_blank" rel="noopener noreferrer">¿QUIENES SOMOS?</a></li>
-                <li><a className="text-decoration-none li-navbar-style" href="http://" target="_blank" rel="noopener noreferrer">CONTACTO</a></li>
+                <li><NavLink className="text-decoration-none li-navbar-style" to="/category/Samsung">Samsung</NavLink></li>
+                <li><NavLink className="text-decoration-none li-navbar-style" to="/category/Apple">Apple</NavLink></li>
+                <li><NavLink className="text-decoration-none li-navbar-style" to="/category/Huawei">Huawei</NavLink></li>
+                <li><NavLink className="text-decoration-none li-navbar-style" to="/category/Nokia">Nokia</NavLink></li>
             </ul>
             <CartWidget />
         </div>
